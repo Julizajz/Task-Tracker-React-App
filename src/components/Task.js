1c89a4659a957e559c72a2f87dd9797818b1f4d5
Juliza <zepeda.juliza@gmail.com>
2023-06-21 13:16:17 -0300

    added react icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import {FaTimes} from 'react-icons'
 const Task = ({ task }) => {
   return (
     <div className="task">
-        <h3>{task.text}</h3>
+        <h3>{task.text} <FaTimes /></h3>
         <p>{task.day}</p>
     </div>
   )
