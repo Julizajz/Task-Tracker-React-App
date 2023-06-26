55bccd2ad1883e9f44561a24b3f8b447b4750860
Juliza <zepeda.juliza@gmail.com>
2023-06-26 18:15:08 -0300

    set usestate to empty array for tasks

--- src/App.js
+++ src/App.js
@@ -5,25 +5,7 @@ import AddTask from './components/AddTask'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([{
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-},
-{
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 5:30pm',
-    reminder: false,
-},
-{
-    id: 3,
-    text: 'Grocery Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-},
-])
+  const [tasks, setTasks] = useState([])
 
   // add task
   const addTask = (task) => {
@@ -49,7 +31,7 @@ function App() {
 
   return (
     <div className="container">
-     <Header />
+     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
      {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
